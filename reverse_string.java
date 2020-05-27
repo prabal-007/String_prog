ad0412135bbac24eb12a08f0828119315b4ba783
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-05-27 14:22:38 +0530

    Update reverse_string.java

--- reverse_string.java
+++ reverse_string.java
@@ -7,7 +7,7 @@ public class reverse_string {
 		String reversestring="";
 		
 		for(int i=string.length()-1;i>=0;i--) {
-			reversestring=reversestring + string.charAt(i);
+			reversestring=reversestring + string.charAt(i);  
 		}
 		System.out.println("Original String : "+string);
 		System.out.println("REversed String : "+reversestring);
